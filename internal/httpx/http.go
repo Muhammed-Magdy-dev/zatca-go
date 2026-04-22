dd672902be1cb60b37684181f8ed0926339fb99f
MuhammedMagdy <mohamed.m.zakaria95@gmail.com>
2026-04-22 21:20:36 +0200

    feat(http):add status code accepted

--- internal/httpx/http.go
+++ internal/httpx/http.go
@@ -52,7 +52,7 @@ func DoRequest(
 
 	bd, _ := io.ReadAll(res.Body)
 
-	if res.StatusCode != http.StatusOK {
+	if res.StatusCode != http.StatusOK || res.StatusCode != http.StatusAccepted {
 		return fmt.Errorf("zatca status %d: %s", res.StatusCode, string(bd))
 	}
 
