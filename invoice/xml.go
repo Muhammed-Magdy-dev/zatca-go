12d7500e7218e2b27a2667515f1242d5e02f1662
MuhammedMagdy <mohamed.m.zakaria95@gmail.com>
2026-04-23 16:28:43 +0200

    feat(xml):Add profile id as parameter

--- invoice/xml.go
+++ invoice/xml.go
@@ -88,6 +88,7 @@ type tmplData struct {
 	CustomerVATNumber        string
 	CustomerRegistrationName string
 	InvoiceTypeName          string
+	ProfileID                string
 }
 
 const invoiceTemplate = `<?xml version="1.0" encoding="UTF-8"?>
@@ -145,7 +146,7 @@ const invoiceTemplate = `<?xml version="1.0" encoding="UTF-8"?>
 </ext:ExtensionContent>
 </ext:UBLExtension>
 </ext:UBLExtensions>
-<cbc:ProfileID>reporting:1.0</cbc:ProfileID>
+<cbc:ProfileID>{{.ProfileID}}</cbc:ProfileID>
 <cbc:ID>{{.ID}}</cbc:ID>
 <cbc:UUID>{{.UUID}}</cbc:UUID>
 <cbc:IssueDate>{{.IssueDate}}</cbc:IssueDate>
@@ -411,6 +412,10 @@ func BuildInvoiceXML(input *InvoiceInput) ([]byte, error) {
 		customerVATNumber = input.Customer.VATNumber
 		customerRegistrationName = input.Customer.RegistrationName
 	}
+	profileID := "reporting:1.0"
+	if !input.IsSimplified {
+		profileID = "clearance:1.0"
+	}
 	data := tmplData{
 		SigningTime:              signingTimeStr,
 		CertificateHash:          input.CertificateHash,
@@ -458,6 +463,7 @@ func BuildInvoiceXML(input *InvoiceInput) ([]byte, error) {
 		CustomerVATNumber:        customerVATNumber,
 		CustomerRegistrationName: customerRegistrationName,
 		InvoiceTypeName:          invoiceTypeName,
+		ProfileID:                profileID,
 	}
 
 	funcMap := template.FuncMap{
